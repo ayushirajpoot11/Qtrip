83cb50161998ea8befc266e4d78147f214d677ab
ayushirajpoot11 <ayushirajput1142@gmail.com>
2023-05-07 14:26:30 +0530

    index file chang

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ window.onscroll = function () {
     if (prevScrollpos > currentScrollPos) {
         document.getElementById("navbar").style.top = "0";
     } else {
-        document.getElementById("navbar").style.top = "-90px";
+        document.getElementById("navbar").style.top = "-190px";
     }
     prevScrollpos = currentScrollPos;
 }
